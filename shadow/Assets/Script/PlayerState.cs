e06a1c88c4d57bfb2e27aec427205e17fee195d5
Tokushige-0411 <cotamomi0411@gmail.com>
2020-10-02 13:37:26 +0900

    addし忘れ

---
+++ shadow/Assets/Script/PlayerState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public enum PlayerState
+    {
+        Idle,       // 立ち状態
+        Walk,       // 歩いている
+        Dash,       // 空中ﾀﾞｯｼｭ
+        Max
+    }
+}
