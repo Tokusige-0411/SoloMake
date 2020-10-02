077dc0012280c23d227cee827ef252632371e532
Tokushige-0411 <cotamomi0411@gmail.com>
2020-10-02 13:36:44 +0900

    dash実装中

--- shadow/Assets/Script/PlayerController.cs
+++ shadow/Assets/Script/PlayerController.cs
@@ -2,28 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace Player
 {
-    private float moveForce_ = 10.0f;
-    private float speed_ = 5.0f;
-    private int key = 0;
-
-    private Vector2 velocity_;
-
-    private Rigidbody2D rigidbody2D_;
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerController : MonoBehaviour
     {
-        rigidbody2D_ = GetComponent<Rigidbody2D>();
-    }
+        [SerializeField] private float speed_ = 3.0f;
+        [SerializeField] private float jumpVec_ = 4.0f;
+        private int dashCnt_ = 1;
+        private float dashSpeed_ = 10.0f;
+        private bool isGround;
+        private PlayerState plState_;
 
-    // Update is called once per frame
-    void Update()
-    {
-        float moveInput = Input.GetAxis("Horizontal");
-        if(moveInput != 0)
+        private Rigidbody2D rigidbody2D_;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            rigidbody2D_.position += new Vector2(speed_ * moveInput, 0.0f) * Time.deltaTime;
+            rigidbody2D_ = GetComponent<Rigidbody2D>();
+            isGround = true;
+            plState_ = PlayerState.Idle;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+            // ﾀﾞｯｼｭ(ﾀﾞｯｼｭ中は移動とかできないようにする)
+            if (Input.GetButtonDown("Dash"))
+            {
+                float dashRad = Vector2.Angle(Vector2.zero, moveInput);
+                Vector2 dashVec = new Vector2(dashSpeed_ * Mathf.Cos(dashRad), dashSpeed_ * Mathf.Sin(dashRad));
+                return;
+            }
+
+            // 左右移動
+            if (moveInput.x != 0)
+            {
+                rigidbody2D_.position += new Vector2(speed_ * moveInput.x, 0.0f) * Time.deltaTime;
+            }
+
+            // ｼﾞｬﾝﾌﾟ
+            if (Input.GetButtonDown("Jump"))
+            {
+                if (isGround)
+                {
+                    rigidbody2D_.AddForce(Vector2.up * jumpVec_, ForceMode2D.Impulse);
+                    isGround = false;
+                }
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            isGround = true;
+            Debug.Log("接地した");
         }
     }
 }
